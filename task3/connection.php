31f3184386c68d82c267f64af18f4156e63d58a3
Arwwa8 <110561558+Arwwa8@users.noreply.github.com>
2022-08-11 23:04:16 +0300

    Add files via upload

---
+++ task3/connection.php
@@ -0,0 +1,25 @@
+
+<?php
+
+$host="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="test2";
+
+
+
+//mysqli
+
+@$con=mysqli_connect($host,$dbuser,$dbpass,$dbname);
+
+if(!$con){
+    
+    echo 'nooo connect';
+    
+}
+
+else{
+	echo 'connect';
+}
+
+?>
